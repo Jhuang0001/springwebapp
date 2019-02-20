75be21eb72afbedc4501db893c52fa8352421eb0
jimmyhuang <jimmyhuang0001@gmail.com>
2019-02-20 13:10:10 -0500

    test

--- src/main/java/guru/springframework/springwebapp/Author.java
+++ src/main/java/guru/springframework/springwebapp/Author.java
@@ -3,6 +3,7 @@ package guru.springframework.springwebapp;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 /*
  * test
@@ -63,5 +64,18 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
